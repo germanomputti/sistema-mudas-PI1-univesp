9b175348139c9546303593a57d4d7c302fb94755
Germano Marcolino Putti <germano@MacBook-Pro-de-Germano.local>
2025-05-23 11:09:41 -0300

    Adiciona formatação brasileira de preços no modelo Especie

--- catalogo/scripts/subir_git.py
+++ catalogo/scripts/subir_git.py
@@ -2,3 +2,4 @@
 #git add catalogo/models.py catalogo/templates/catalogo/mudas.html catalogo/scripts/subir_git.py
 #git commit -m "Adiciona formatação brasileira de preços no modelo Especie"
 #git push
+

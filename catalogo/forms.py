ca20c11e2f1ae2b038841758358df38c90c9d99c
Germano Marcolino Putti <germano@MacBook-Pro-de-Germano.local>
2025-05-23 10:44:44 -0300

    Ordena espécies alfabeticamente no formulário de cadastro

--- catalogo/forms.py
+++ catalogo/forms.py
@@ -5,3 +5,7 @@ class MudaPlantadaForm(forms.ModelForm):
     class Meta:
         model = MudaPlantada
         fields = ['especie', 'data_plantio', 'quantidade']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Ordena as espécies alfabeticamente pelo nome
+        self.fields['especie'].queryset = Especie.objects.order_by('nome')
